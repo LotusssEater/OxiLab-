1ec656027fa0a6290f3d7291fab50c1d7e468511
Vina del Mar <vina@duoc.acad>
2019-11-06 11:37:30 -0300

    Actualización, clase solo 06/11/2019 - 11:37

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        permissions = (
+            ('admin', ('Administrador OxiLab')),
+            ('artista', ('Artista OxiLab')),
+        )
--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,17 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/Inicio.html', {'posts': posts })
+    user = request.user
+    if user.has_perm('blog.admin'):
+        posts = Post.objects.filter(
+            published_date_lte=timezone.now()).orderby('published_date')
+        return render(request, 'blog/post_list.html', {'posts': posts})
+    else:
+        return render(request, 'blog/Inicio.html')
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+    
     
 
