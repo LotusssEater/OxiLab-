a83f02ba9fb438496279c3aad04e1515102f73b3
Esteban Pizarro <estebanpizarro790@gmail.com>
2019-11-29 00:07:03 -0400

    CRUD POST

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
-from django.contrib import admin
 
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('admin/', admin.site.urls, name='adm'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('Inicio/', views.Inicio, name='Inicio'),
     path('AboutUs/', views.AboutUs, name='AboutUs'),
     path('Comunity/', views.Comunity, name='Comunity'),
