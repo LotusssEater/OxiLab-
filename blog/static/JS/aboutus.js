63f27ccd9f5346bdf0cbbb1ec24f6a3e75a084b0
Esteban Pizarro <estebanpizarro790@gmail.com>
2019-11-30 14:30:23 -0400

    .

--- blog/static/JS/aboutus.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-function AbrirMenu(){
-    document.getElementById('sidebar').classList.toggle('active');
-    document.getElementById('lateral_derecho').classList.toggle('active');
-}
-
-function myFunction(x) {
-    x.classList.toggle("change");
-}
--- blog/static/JS/comunity.js
+++
@@ -1,7 +0,0 @@
-function AbrirMenu(){
-    document.getElementById('sidebar').classList.toggle('active');
-    document.getElementById('lateral_derecho').classList.toggle('active');
-}
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
