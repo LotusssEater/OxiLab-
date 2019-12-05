7f00fe0bab2e2104bfa7457db011d5892d5d22de
Esteban Pizarro <estebanpizarro790@gmail.com>
2019-12-05 19:52:47 -0400

    Andan Funando Gente Inocentee!

--- blog/static/JS/Containers_Interact.js
+++ blog/static/JS/Containers_Interact.js
@@ -5,12 +5,5 @@ function Reproducir(repro){
     // document.getElementById(song).classList.toggle("active");
 
 }
-function ParaArtistas(){ 
-    alert("aqui se redirecciona al perfil del artista");
-    /*document.getElementById("reproductor_visualizador").classList.toggle("active");*/
-}
-function ParaSamples(){ 
-    alert("aqui se redirecciona al del Sample pack");
-    /*document.getElementById("reproductor_visualizador").classList.toggle("active");*/
-}
-var samples = ["Lofi","Electronica","Hip Hop", "Trap","Alternativa"];
+
+
